0e33019ef47f703e64c7a986d93221c990fbdea4
Chatter2305 <boredbuck@gmail.com>
2024-05-23 21:01:05 +0530

    blockchain.py

---
+++ blockchain/blockchain.py
@@ -0,0 +1,17 @@
+from genesis_block import GenesisBlock
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return GenesisBlock()
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
