3cc1e85a02c3447ec30e8d595f2e81a507a68139
Chatter2305 <boredbuck@gmail.com>
2024-05-23 21:09:45 +0530

    blockchain.py

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -15,3 +15,26 @@ class Blockchain:
         new_block.previous_hash = self.get_latest_block().hash
         new_block.hash = new_block.calculate_hash()
         self.chain.append(new_block)
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.pending_transactions = []
+
+    def create_genesis_block(self):
+        return GenesisBlock()
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def add_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def mine_pending_transactions(self, miner_address):
+        block = Block(len(self.chain), self.get_latest_block().hash, self.pending_transactions)
+        self.add_block(block)
+        self.pending_transactions = [{"from": "network", "to": miner_address, "amount": 1}]
